68d612a76e1446b9e792f27066fe93b05565315c
Apna Code <asuszenbook298@gmail.com>
2025-12-14 22:17:54 +0530

    Add Gfg Sum solutions

---
+++ Arrays/Gfg.SumSubarray.java
@@ -0,0 +1,11 @@
+package Arrays;
+class Solution {
+    public int subarraySum(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            sum += arr[i] * (i + 1) * (n - i);
+        }
+        return sum;
+    }
+}
