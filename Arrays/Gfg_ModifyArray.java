a87292b7b8391d31612009076acf5dc6b7084192
Apna Code <asuszenbook298@gmail.com>
2025-12-15 00:49:53 +0530

    Add Gfg_Modify Array Solutions

--- Arrays/Gfg_ModifyArray.java
+++ Arrays/Gfg_ModifyArray.java
@@ -3,8 +3,8 @@ class Solution{
     public static void modifyArray(int[] arr) {
         int n = arr.length;
         for(int i = 0 ; i < n; i++){
-            int j = arr[j] % n;
-            arr[i] += i * n; 
+            int j = arr[i] % n;
+            arr[j] += i * n; 
                }
         for(int i = 0 ; i < n; i++){
             arr[i] /= n ;
