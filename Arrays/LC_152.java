dddc800314a19f62f20de95697b2a416f37f0bd8
Apna Code <asuszenbook298@gmail.com>
2025-12-18 14:13:49 +0530

    Add LC_152 solutions

--- Arrays/LC_152.java
+++ Arrays/LC_152.java
@@ -1,5 +1,7 @@
 package Arrays;
 class Solution {
+
+    // Solution 1 : Using two traversals
     public int maxProduct(int[] nums) {
         int n = nums.length;
         int maxProd = Integer.MIN_VALUE;
@@ -24,4 +26,29 @@ class Solution {
 
         return maxProd;    
     }
+
+    // Solution 2 : Using max and min product
+    public int maxProductK(int[] nums) {
+        int n = nums.length;
+        int maxProd = nums[0];
+        int minProd = nums[0];  
+        int ans = nums[0]; 
+
+        for(int i = 1; i < n; i++){
+            int num = nums[i];
+
+            if(num < 0){
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            maxProd = Math.max(num , maxProd * num);
+            minProd = Math.min(num , minProd * num);
+
+            ans = Math.max(ans , maxProd);
+        } 
+        return ans;
+    }
 }
+
