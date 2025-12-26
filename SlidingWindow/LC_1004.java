6a0c00f17ea7f4d1050380306868c93a8b828139
Apna Code <asuszenbook298@gmail.com>
2025-12-26 18:48:06 +0530

    Add LC_1004 solutions

---
+++ SlidingWindow/LC_1004.java
@@ -0,0 +1,21 @@
+package SlidingWindow;
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0 , right = 0;
+        int ans = 0;
+        int zeros = 0;
+        while(right < nums.length){
+            if(nums[right] == 0) zeros++;
+
+            while(zeros > k){
+                if(nums[left] == 0) zeros--;
+                left++;
+            }
+            ans = Math.max(ans , right - left + 1);
+            right++;
+
+        }
+        return ans;
+        
+    }
+}
