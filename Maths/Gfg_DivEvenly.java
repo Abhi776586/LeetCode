a2736533f62ab84343a9f6d0b82eac135d094b16
Apna Code <asuszenbook298@gmail.com>
2026-01-15 17:01:36 +0530

    k < n

---
+++ Maths/Gfg_DivEvenly.java
@@ -0,0 +1,19 @@
+public class Gfg_DivEvenly {
+    // User function Template for Java
+
+class Solution {
+    static int evenlyDivides(int n) {
+        int count = 0;
+        int original = n;
+        while(n > 0){
+        int digit = n % 10;
+        if(digit != 0 && original % digit == 0){
+            count++;
+        }
+        n = n/10;
+        }
+        return count;
+    }
+}
+    
+}
---
+++ Maths/Gfg_SqCount.java
@@ -0,0 +1,11 @@
+public class Gfg_SqCount {
+    // User function Template for Java
+
+class Solution {
+    static int countSquares(int n) {
+        return (int) Math.sqrt(n - 1);
+    }
+}
+    
+}
+
